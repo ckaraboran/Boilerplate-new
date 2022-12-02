9620695a66f924805b74f8e370595349d851c040
Can Karaboran <can.karaboran@carnext.com>
2022-12-02 14:27:11 +0100

    update automapper profile

--- Libraries/Boilerplate.Application/Mappings/AutoMapperProfile.cs
+++ Libraries/Boilerplate.Application/Mappings/AutoMapperProfile.cs
@@ -2,12 +2,11 @@ using Boilerplate.Application.Commands;
 
 namespace Boilerplate.Application.Mappings;
 
-public class AutoMapperProfile: Profile
+public class AutoMapperProfile : Profile
 {
     public AutoMapperProfile()
     {
-        CreateMap<DummyDto, CreateDummyCommand>().ReverseMap();
-        CreateMap<CreateDummyCommand, Dummy>();
+        CreateMap<Dummy, CreateDummyCommand>().ReverseMap();
         CreateMap<DummyDto, UpdateDummyCommand>().ReverseMap();
     }
 }
