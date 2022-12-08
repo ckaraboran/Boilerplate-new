15b1dca0afda60e3fe73c767d939f77a1b07b7f7
ckaraboran <karabaron@gmail.com>
2022-12-09 00:47:59 +0100

    #39 add logging behaviour pipeline to mediator

--- Libraries/Boilerplate.Application/DependencyInjection.cs
+++ Libraries/Boilerplate.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using Boilerplate.Application.Behaviours;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,6 +10,7 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(Assembly.GetExecutingAssembly());
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
         return services;
     }
 }
