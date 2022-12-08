c8701cb61aff11a95b9c647a1fa875350ff31904
Can Karaboran <can.karaboran@carnext.com>
2022-12-08 11:35:16 +0100

    add surpress for sonarcloud

--- Libraries/Boilerplate.Domain/Models/UserConstants.cs
+++ Libraries/Boilerplate.Domain/Models/UserConstants.cs
@@ -1,13 +1,14 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace Boilerplate.Domain.Models;
 
 public static class UserConstants
 {
     public static IEnumerable<UserModel> Users => UserCollection;
 
+    [SuppressMessage("SonarLint", "S2068", Justification = "Ignored intentionally as a boilerplate app")]
     private static readonly List<UserModel> UserCollection = new()
     {
         new UserModel { Username = "ckaraboran", Password = "ckaraboran", Role = "Admin" }
     };
-    
-    
 }
